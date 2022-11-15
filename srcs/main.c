73968944b82323b8a0f9f27029cf1ab4667f08ac
Dagmar Kramer <dkramer@f1r2s1.codam.nl>
2022-11-15 13:56:59 +0100

    parsing working without leaks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dkramer <dkramer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/09 10:34:24 by dkramer       #+#    #+#                 */
-/*   Updated: 2022/11/14 14:06:51 by dkramer       ########   odam.nl         */
+/*   Updated: 2022/11/15 11:31:10 by dkramer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 		mlx.map_filename = argv[1];
 		if (map_parse(&mlx) == 1)
 		{
-			// system ("leaks cub3D");
+			system ("leaks cub3D");
 			return (1);
 		}
 		//debug_print_map(&mlx);
@@ -32,6 +32,6 @@ int	main(int argc, char **argv)
 		return (error_msg_ret("Incorrect number of arguments.", 1));
 	}
 	map_free(mlx.map, &mlx);
-	// system ("leaks cub3D");
+	system ("leaks cub3D");
 	return (0);
 }
--- srcs/parsing/variables.c
+++ srcs/parsing/variables.c
@@ -6,7 +6,7 @@
 /*   By: dkramer <dkramer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/14 13:50:36 by dkramer       #+#    #+#                 */
-/*   Updated: 2022/11/15 11:10:05 by dkramer       ########   odam.nl         */
+/*   Updated: 2022/11/15 13:51:53 by dkramer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,62 +25,103 @@ void	free_split(char **split)
 	free (split);
 }
 
+int	get_colors(t_mlx *mlx, char	**split_line)
+{
+	char	**split_color;
+
+	if ((!ft_strncmp("C", split_line[0], 1) && mlx->ccolor)
+		|| (!ft_strncmp("F", split_line[0], 1) && mlx->fcolor))
+	{
+		free_split(split_line);
+		return (error_msg_ret("Double variable in map.", 1));
+	}
+	split_color = ft_split(split_line[1], ',');
+	if (!split_color[0] || !split_color[1] || !split_color[2])
+	{
+		free_split(split_color);
+		free_split(split_line);
+		return (error_msg_ret("Wrong format variable in map.", 1));
+	}
+	if (!ft_strncmp("F", split_line[0], 1))
+	{
+		mlx->fcolor = ft_atoi(split_color[0]) << 24 | ft_atoi(split_color[1])
+			<< 16 | ft_atoi(split_color[2]) << 8 | 0xff;
+		free_split(split_color);
+	}
+	if (!ft_strncmp("C", split_line[0], 1))
+	{
+		mlx->ccolor = ft_atoi(split_color[0]) << 24 | ft_atoi(split_color[1])
+			<< 16 | ft_atoi(split_color[2]) << 8 | 0xff;
+		free_split(split_color);
+	}
+	return (0);
+}
+
 int	get_one_variable(t_mlx *mlx, char *line)
 {
 	char	**split_line;
-	char	**split_color;
-
-	split_color = NULL;
+	
 	split_line = ft_split(line, ' ');
+	if (!ft_strncmp("NO", split_line[0], 2) || !ft_strncmp("SO", split_line[0], 2) || !ft_strncmp("WE", split_line[0], 2) || !ft_strncmp("EA", split_line[0], 2) || !ft_strncmp("F", split_line[0], 1) || !ft_strncmp("C", split_line[0], 1))
+	{
+		if (!split_line[1])
+		{
+			free_split(split_line);
+			return (error_msg_ret("Wrong format variable in map.", 1));
+		}
+	}
 	if (!ft_strncmp("NO", split_line[0], 2))
 	{
 		if (mlx->NO)
+		{
+			free_split(split_line);
 			return (error_msg_ret("Double variable in map.", 1));
-		mlx->NO= split_line[1];
+		}
+		mlx->NO = split_line[1];
 	}
 	else if (!ft_strncmp("SO", split_line[0], 2))
 	{
 		if (mlx->SO)
+		{
+			free_split(split_line);
 			return (error_msg_ret("Double variable in map.", 1));
+		}
 		mlx->SO = split_line[1];
 	}
 	else if (!ft_strncmp("WE", split_line[0], 2))
 	{
 		if (mlx->WE)
+		{
+			free_split(split_line);
 			return (error_msg_ret("Double variable in map.", 1));
+		}
 		mlx->WE = split_line[1];
 	}
 	else if (!ft_strncmp("EA", split_line[0], 2))
 	{
 		if (mlx->EA)
+		{
+			free_split(split_line);
 			return (error_msg_ret("Double variable in map.", 1));
+		}
 		mlx->EA = split_line[1];
 	}
-	else if (!ft_strncmp("F", split_line[0], 2))
+	else if (!ft_strncmp("F", split_line[0], 1) || !ft_strncmp("C", split_line[0], 1))
 	{
-		if (mlx->fcolor)
-			return (error_msg_ret("Double variable in map.", 1));
-		split_color = ft_split(split_line[1], ',');
-		mlx->fcolor = ft_atoi(split_color[0]) << 24 | ft_atoi(split_color[1]) << 16 | ft_atoi(split_color[2]) << 8 | 0xff;
-		free_split(split_color);
-	}
-	else if (!ft_strncmp("C", split_line[0], 2))
-	{
-		if (mlx->ccolor)
-			return (error_msg_ret("Double variable in map.", 1));
-		split_color = ft_split(split_line[1], ',');
-		mlx->ccolor = ft_atoi(split_color[0]) << 24 | ft_atoi(split_color[1]) << 16 | ft_atoi(split_color[2]) << 8 | 0xff;
-		free_split(split_color);
+		if (get_colors(mlx, split_line) == 1)
+			return (1);
 	}
 	else
 	{
-		if (ft_strncmp("C", split_line[0], 2) || ft_strncmp("F", split_line[0], 2) || ft_strncmp("EA", split_line[0], 2) || ft_strncmp("WE", split_line[0], 2) || ft_strncmp("SO", split_line[0], 2) || ft_strncmp("NO", split_line[0], 2))
+		if (split_line[1] && (ft_strncmp("C", split_line[0], 2) || ft_strncmp("F", split_line[0], 2) || ft_strncmp("EA", split_line[0], 2) || ft_strncmp("WE", split_line[0], 2) || ft_strncmp("SO", split_line[0], 2) || ft_strncmp("NO", split_line[0], 2)))
+		{
+			free_split(split_line);
 			return (error_msg_ret("Unknown variable in map.", 1));
+		}
 		if (ft_strrchr("01NESW ", split_line[0][0]))
 			mlx->stop = 1;
 	}
-	free (split_line[0]);
-	free (split_line);
+	free_split(split_line);
 	return (0);
 }
 
@@ -99,11 +140,15 @@ int	get_variables(t_mlx *mlx, char *line)
 		if (ft_strncmp(line, "", ft_strlen(line)) != 0)
 		{
 			if (mlx->stop == 1)
+			{
+				free (line);
 				break;
-            if (mlx->fcolor && mlx->ccolor && mlx->NO && mlx->SO && mlx->WE && mlx->EA)
-                break ;
+			}
 			if (get_one_variable(mlx, line) == 1)
+			{
+				free (line);
 				return (1);
+			}
         }
 		if (line)
 			free (line);
