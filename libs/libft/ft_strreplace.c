b17d229a4256f31e9a9ce046a81f06205f0747a1
Dagmar Kramer <dkramer@f1r5s7.codam.nl>
2023-01-12 10:49:17 +0100

    libft norm

--- libs/libft/ft_strreplace.c
+++ libs/libft/ft_strreplace.c
@@ -6,20 +6,20 @@
 /*   By: dkramer <dkramer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/02 14:18:26 by dkramer       #+#    #+#                 */
-/*   Updated: 2022/11/02 14:29:05 by dkramer       ########   odam.nl         */
+/*   Updated: 2023/01/12 10:41:40 by dkramer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strreplace(char *dst, int before, int after)
+char	*ft_strreplace(char *dst, int before, int after)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(dst[i])
-    {
-        if (dst[i] == before)
-            dst[i] = after;
-        i++;
-    }
-    return (dst);
+	i = 0;
+	while (dst[i])
+	{
+		if (dst[i] == before)
+			dst[i] = after;
+		i++;
+	}
+	return (dst);
 }
